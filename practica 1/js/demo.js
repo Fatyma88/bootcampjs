0bf49686717e582fbfd09041edc5dc4493c2e49f
Fatima <fatima.ocaa@yahoo.com>
2023-03-09 11:30:53 +0100

    1

---
+++ practica 1/js/demo.js
@@ -0,0 +1,4 @@
+document.getElementById("name").value = "Fatima";
+document.getElementById("lastName").value = "Ocaña";
+// diff
+document.getElementById("avatar").src = "file:///C:/Users/Usuario/bootcampjs/bootcampjs/practica%201/img/undraw_female_avatar_efig%20(1).svg";
