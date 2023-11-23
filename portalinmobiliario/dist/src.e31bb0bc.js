60a01af2e6e6761efe50af39362e319634c555f3
Fatima <fatima.ocaa@yahoo.com>
2023-11-23 23:36:58 +0100

    correccion

--- portalinmobiliario/dist/src.e31bb0bc.js
+++ portalinmobiliario/dist/src.e31bb0bc.js
@@ -2938,7 +2938,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57631" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51569" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
